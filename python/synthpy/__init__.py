a2913ad79610d0a563c3c7a3dd5b94ce55f7a85f
David Nidever <dnidever@lsst.org>
2023-01-03 14:12:34 -0700

    moved atmopsheres to atmosphere and linelists to linelist

--- python/synthpy/__init__.py
+++ python/synthpy/__init__.py
@@ -1,2 +1,2 @@
-__all__ = ["utils","atmospheres","linelists","spectrum","spectrumizer","korg","atomic"]
+__all__ = ["utils","atmosphere","linelist","spectrum","spectrumizer","korg","atomic"]
 __version__ = '1.0.0'
